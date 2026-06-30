7e21cb7cb5ec6e123072b004f88963b8ee4201db
azrisafwan95 <azrisafwan1@gmail.com>
2026-06-30 15:04:22 +0800

    Delete src/data/presets.ts

--- src/data/presets.ts
+++
@@ -1,9 +0,0 @@
-import { AIRCOND_DATA } from './presets/aircond';
-import { LORI_DATA } from './presets/lori';
-import { CLEANING_DATA } from './presets/cleaning';
-
-export const ALL_PRESETS: Record<string, any> = {
-  aircond: AIRCOND_DATA,
-  lori: LORI_DATA,
-  cleaning: CLEANING_DATA,
-};
